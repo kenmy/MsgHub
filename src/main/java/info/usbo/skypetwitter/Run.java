660f8f6b36446522482f1a7792a0f676e393885a
Evgeny_Kincharov <Evgeny_Kincharov@epam.com>
2015-11-20 13:16:22 +0400

    Added formatter and formatted java sources

--- src/main/java/info/usbo/skypetwitter/Run.java
+++ src/main/java/info/usbo/skypetwitter/Run.java
@@ -54,177 +54,177 @@ import twitter4j.URLEntity;
  * @author Pavel_Goncharenko
  */
 public class Run {
-	
-	private static Integer bChanged;
-	
-	private static ArrayList<Long> twitter_ids = new ArrayList<Long>();
-	private static ArrayList<Long> vk_ids = new ArrayList<Long>();
-	private static ArrayList<VK> vk = new ArrayList<VK>();
-	
-	private static String data_dir;
-	
-	public static void main(String[] args) throws SkypeException {
-		System.out.println("Working Directory = " + System.getProperty("user.dir"));
-		Properties props = (Properties) System.getProperties().clone();
-		String prop_dir = System.getProperty("user.dir");
-		loadProperties(props, prop_dir + "\\twitter4j.properties");
+
+    private static Integer bChanged;
+
+    private static ArrayList<Long> twitter_ids = new ArrayList<Long>();
+    private static ArrayList<Long> vk_ids = new ArrayList<Long>();
+    private static ArrayList<VK> vk = new ArrayList<VK>();
+
+    private static String data_dir;
+
+    public static void main(String[] args) throws SkypeException {
+        System.out.println("Working Directory = " + System.getProperty("user.dir"));
+        Properties props = (Properties) System.getProperties().clone();
+        String prop_dir = System.getProperty("user.dir");
+        loadProperties(props, prop_dir + "\\twitter4j.properties");
         loadProperties(props, prop_dir + "\\skype.properties");
         loadProperties(props, prop_dir + "\\app.properties");
-        String chat_group_id = props.getProperty("skype.chat_group_id");		
+        String chat_group_id = props.getProperty("skype.chat_group_id");
         String twitter_user_id = props.getProperty("twitter.user");
-		data_dir = props.getProperty("data.dir");      
-		Integer twitter_timeout = Integer.parseInt(props.getProperty("twitter.timeout"));
-		System.out.println("Twitter user: " + twitter_user_id);
-		System.out.println("Twitter timeout: " + twitter_timeout);
-		if ("".equals(twitter_user_id)) {
-			return;
-		}
-		if (load_file() == 0) {
-			System.out.println("File not found");
-			return;
-		}
-		while (true) {
-			bChanged = 0;
-			SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy HH:mm");
-			System.out.println("Looking at " + sdf.format(Calendar.getInstance().getTime()));
-			Chat ch = Skype.chat(chat_group_id);
-			Twitter twitter = new TwitterFactory().getInstance();
-			try {
-				List<Status> statuses;
-				statuses = twitter.getUserTimeline(twitter_user_id);
-				String sText;
-				for (Status status : statuses) {
-					Date d = status.getCreatedAt();
-					// Добавим  часов
-					Calendar cal = Calendar.getInstance(); 
-					cal.setTime(d);
-					cal.add(Calendar.HOUR_OF_DAY, 7); 
-					d = cal.getTime();
-					sText = "@" + status.getUser().getScreenName() + " от " + sdf.format(d) + " ( https://twitter.com/" + twitter_user_id + "/status/" + status.getId() + " ): \r\n" + status.getText() + "\r\n***";
-					for (URLEntity e : status.getURLEntities())	{
-						sText = sText.replaceAll(e.getURL(), e.getExpandedURL());
-					}
-					for (MediaEntity e : status.getMediaEntities()) {
-						sText = sText.replaceAll(e.getURL(), e.getMediaURL());
-					}						
-					if (twitter_ids.indexOf(status.getId()) == -1) {
-						System.out.println(sText);
-						ch.send(sText);
-						twitter_ids.add(status.getId());
-						bChanged = 1;
-					}
-				}
-			} catch (TwitterException te) {
-				te.printStackTrace();
-				System.out.println("Failed to get timeline: " + te.getMessage());
-				//System.exit(-1);
-			} catch (SkypeException ex) {
-				ex.printStackTrace();
-				System.out.println("Failed to send message: " + ex.getMessage());
-			}
-			
-			// VK
-			try {
-				vk();
-				for (VK v : vk) {
-					if (vk_ids.indexOf(v.getId()) == -1) {
-						Date d = v.getDate();
-						// Добавим  часов
-						Calendar cal = Calendar.getInstance(); 
-						cal.setTime(d);
-						cal.add(Calendar.HOUR_OF_DAY, 7); 
-						d = cal.getTime();
-						String sText = "@Depersonilized (VK) от " + sdf.format(d) + " ( http://vk.com/Depersonilized?w=wall-0_" + v.getId() + " ): \r\n" + v.getText();
-						if (!"".equals(v.getAttachment())) {
-							sText += "\r\n" + v.getAttachment();
-						}
-						sText += "\r\n***";
-						System.out.println(sText);
-						ch.send(sText);
-						vk_ids.add(v.getId());
-						bChanged = 1;
-					}
-				}
-			} catch (ParseException e) {
-				e.printStackTrace();
-				System.out.println("Failed to get vk: " + e.getMessage());
-				//System.exit(-1);
-			} catch (SkypeException ex) {
-				ex.printStackTrace();
-				System.out.println("Failed to send message: " + ex.getMessage());
-			}
-			if (bChanged == 1) {
-				save_file();
-			}
-			try {
-				Thread.sleep(1000 * 60 * twitter_timeout);
-			} catch (InterruptedException ex) {
-				ex.printStackTrace();
-				System.out.println("Failed to sleep: " + ex.getMessage());
-			}
-		}
+        data_dir = props.getProperty("data.dir");
+        Integer twitter_timeout = Integer.parseInt(props.getProperty("twitter.timeout"));
+        System.out.println("Twitter user: " + twitter_user_id);
+        System.out.println("Twitter timeout: " + twitter_timeout);
+        if ("".equals(twitter_user_id)) {
+            return;
+        }
+        if (load_file() == 0) {
+            System.out.println("File not found");
+            return;
+        }
+        while (true) {
+            bChanged = 0;
+            SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy HH:mm");
+            System.out.println("Looking at " + sdf.format(Calendar.getInstance().getTime()));
+            Chat ch = Skype.chat(chat_group_id);
+            Twitter twitter = new TwitterFactory().getInstance();
+            try {
+                List<Status> statuses;
+                statuses = twitter.getUserTimeline(twitter_user_id);
+                String sText;
+                for (Status status : statuses) {
+                    Date d = status.getCreatedAt();
+                    // Добавим часов
+                    Calendar cal = Calendar.getInstance();
+                    cal.setTime(d);
+                    cal.add(Calendar.HOUR_OF_DAY, 7);
+                    d = cal.getTime();
+                    sText = "@" + status.getUser().getScreenName() + " от " + sdf.format(d) + " ( https://twitter.com/"
+                            + twitter_user_id + "/status/" + status.getId() + " ): \r\n" + status.getText() + "\r\n***";
+                    for (URLEntity e : status.getURLEntities()) {
+                        sText = sText.replaceAll(e.getURL(), e.getExpandedURL());
+                    }
+                    for (MediaEntity e : status.getMediaEntities()) {
+                        sText = sText.replaceAll(e.getURL(), e.getMediaURL());
+                    }
+                    if (twitter_ids.indexOf(status.getId()) == -1) {
+                        System.out.println(sText);
+                        ch.send(sText);
+                        twitter_ids.add(status.getId());
+                        bChanged = 1;
+                    }
+                }
+            } catch (TwitterException te) {
+                te.printStackTrace();
+                System.out.println("Failed to get timeline: " + te.getMessage());
+                // System.exit(-1);
+            } catch (SkypeException ex) {
+                ex.printStackTrace();
+                System.out.println("Failed to send message: " + ex.getMessage());
+            }
+
+            // VK
+            try {
+                vk();
+                for (VK v : vk) {
+                    if (vk_ids.indexOf(v.getId()) == -1) {
+                        Date d = v.getDate();
+                        // Добавим часов
+                        Calendar cal = Calendar.getInstance();
+                        cal.setTime(d);
+                        cal.add(Calendar.HOUR_OF_DAY, 7);
+                        d = cal.getTime();
+                        String sText = "@Depersonilized (VK) от " + sdf.format(d) + " ( http://vk.com/Depersonilized?w=wall-0_"
+                                + v.getId() + " ): \r\n" + v.getText();
+                        if (!"".equals(v.getAttachment())) {
+                            sText += "\r\n" + v.getAttachment();
+                        }
+                        sText += "\r\n***";
+                        System.out.println(sText);
+                        ch.send(sText);
+                        vk_ids.add(v.getId());
+                        bChanged = 1;
+                    }
+                }
+            } catch (ParseException e) {
+                e.printStackTrace();
+                System.out.println("Failed to get vk: " + e.getMessage());
+                // System.exit(-1);
+            } catch (SkypeException ex) {
+                ex.printStackTrace();
+                System.out.println("Failed to send message: " + ex.getMessage());
+            }
+            if (bChanged == 1) {
+                save_file();
+            }
+            try {
+                Thread.sleep(1000 * 60 * twitter_timeout);
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+                System.out.println("Failed to sleep: " + ex.getMessage());
+            }
+        }
     }
 
-	public static void save_file() {
-		try{
-         FileOutputStream fos= new FileOutputStream(data_dir + "\\twitter_ids.data");
-         ObjectOutputStream oos= new ObjectOutputStream(fos);
-         oos.writeObject(twitter_ids);
-         oos.close();
-         fos.close();
-       }catch(IOException ioe){
+    public static void save_file() {
+        try {
+            FileOutputStream fos = new FileOutputStream(data_dir + "\\twitter_ids.data");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(twitter_ids);
+            oos.close();
+            fos.close();
+        } catch (IOException ioe) {
             ioe.printStackTrace();
         }
-		
-		try{
-         FileOutputStream fos= new FileOutputStream(data_dir + "\\vk_ids.data");
-         ObjectOutputStream oos= new ObjectOutputStream(fos);
-         oos.writeObject(vk_ids);
-         oos.close();
-         fos.close();
-       }catch(IOException ioe){
+
+        try {
+            FileOutputStream fos = new FileOutputStream(data_dir + "\\vk_ids.data");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(vk_ids);
+            oos.close();
+            fos.close();
+        } catch (IOException ioe) {
             ioe.printStackTrace();
         }
-	}
-	
-	@SuppressWarnings("unchecked")
+    }
+
+    @SuppressWarnings("unchecked")
     public static int load_file() {
-		// TWITTER
-		try
-        {
+        // TWITTER
+        try {
             FileInputStream fis = new FileInputStream(data_dir + "\\twitter_ids.data");
             ObjectInputStream ois = new ObjectInputStream(fis);
             twitter_ids = (ArrayList<Long>) ois.readObject();
             ois.close();
             fis.close();
-         }catch(IOException ioe){
-             ioe.printStackTrace();
-             return 0;
-          }catch(ClassNotFoundException c){
-             System.out.println("Class not found");
-             c.printStackTrace();
-             return 0;
-          }
-		// VK
-		try
-        {
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            return 0;
+        } catch (ClassNotFoundException c) {
+            System.out.println("Class not found");
+            c.printStackTrace();
+            return 0;
+        }
+        // VK
+        try {
             FileInputStream fis = new FileInputStream(data_dir + "\\vk_ids.data");
             ObjectInputStream ois = new ObjectInputStream(fis);
             vk_ids = (ArrayList<Long>) ois.readObject();
             ois.close();
             fis.close();
-         }catch(IOException ioe){
-             ioe.printStackTrace();
-             return 0;
-          }catch(ClassNotFoundException c){
-             System.out.println("Class not found");
-             c.printStackTrace();
-             return 0;
-          }
-		return 1;
-	}
-	
-	private static boolean loadProperties(Properties props, String path) {
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+            return 0;
+        } catch (ClassNotFoundException c) {
+            System.out.println("Class not found");
+            c.printStackTrace();
+            return 0;
+        }
+        return 1;
+    }
+
+    private static boolean loadProperties(Properties props, String path) {
         FileInputStream fis = null;
         try {
             File file = new File(path);
@@ -245,28 +245,28 @@ public class Run {
         }
         return false;
     }
-	
-	private static void vk() throws ParseException {
-		String url = "https://api.vk.com/method/wall.get?v=5.28&domain=Depersonilized&filter=owner&extended=1";
-		String line = "";
-		try {
-			URL url2 = new URL(url);
-			BufferedReader reader = new BufferedReader(new InputStreamReader(url2.openStream(), "UTF-8"));
-			line = reader.readLine();
-			reader.close();
 
-		} catch (MalformedURLException e) {
-			// ...
-		} catch (IOException e) {
-			// ...
-		}
-		JSONObject json = (JSONObject)new JSONParser().parse(line);
-		json = (JSONObject)new JSONParser().parse(json.get("response").toString());
-		JSONArray jsona = (JSONArray) new JSONParser().parse(json.get("items").toString());
-		vk.clear();
-		for (int i = 0; i < jsona.size(); i++) {
-			vk.add(new VK(jsona.get(i).toString()));
-		}
-	}
-	
+    private static void vk() throws ParseException {
+        String url = "https://api.vk.com/method/wall.get?v=5.28&domain=Depersonilized&filter=owner&extended=1";
+        String line = "";
+        try {
+            URL url2 = new URL(url);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url2.openStream(), "UTF-8"));
+            line = reader.readLine();
+            reader.close();
+
+        } catch (MalformedURLException e) {
+            // ...
+        } catch (IOException e) {
+            // ...
+        }
+        JSONObject json = (JSONObject) new JSONParser().parse(line);
+        json = (JSONObject) new JSONParser().parse(json.get("response").toString());
+        JSONArray jsona = (JSONArray) new JSONParser().parse(json.get("items").toString());
+        vk.clear();
+        for (int i = 0; i < jsona.size(); i++) {
+            vk.add(new VK(jsona.get(i).toString()));
+        }
+    }
+
 }
